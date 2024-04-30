08de7b696a933543691ff0e49c95c251104f492f
dev-safiurrehman <dev.safiurrehman@gmail.com>
2024-04-30 23:33:18 +0500

    plugin revised

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,100 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/auth_page.dart';
-// import 'pages/login_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_application_1/pages/login_or_register_page.dart';
-import 'package:flutter_application_1/pages/vision.dart';
-import 'firebase_options.dart';
+import 'dart:io' as io;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await  Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+import 'package:flutter/material.dart';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'package:flutter_application_1/ultralytics_yolo/ultralytics_yolo.dart';
+import 'package:flutter_application_1/ultralytics_yolo/yolo_model.dart';
+
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = UltralyticsYoloCameraController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        splash: 
-          const Text('FRUIT VIS', 
-          style: TextStyle(color: Colors.orange, fontSize: 22, fontWeight: FontWeight.bold)),
-          duration: 2000,
-          splashTransition: SplashTransition.fadeTransition,
-          backgroundColor: Colors.white
-      , 
-      nextScreen: const AuthPage()
-      )
+      home: Scaffold(
+        body: FutureBuilder<bool>(
+          future: _checkCameraPermission(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData || !snapshot.data!) {
+              return Center(child: CircularProgressIndicator());
+            }
+            return FutureBuilder<ObjectDetector>(
+                future: _initObjectDetectorWithLocalModel(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState != ConnectionState.done) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  final predictor = snapshot.data;
+                  if (predictor == null) {
+                    return Center(child: Text("Failed to load the model"));
+                  }
+
+                  return UltralyticsYoloCameraPreview(
+                    controller: controller,
+                    predictor: predictor,
+                    onCameraCreated: () => predictor.loadModel(useGpu: true),
+                  );
+                });
+          },
+        ),
+      ),
     );
+  }
+
+  Future<ObjectDetector> _initObjectDetectorWithLocalModel() async {
+    try {
+      final modelPath = await _copyModelToLocal('assets/last_int8.tflite');
+      final metadataPath = await _copyModelToLocal('assets/metadata.yaml');
+      final model = LocalYoloModel(
+        id: '',
+        task: Task.detect,
+        format: Format.tflite,
+        modelPath: modelPath,
+        metadataPath: metadataPath,
+      );
+      return ObjectDetector(model: model);
+    } catch (e) {
+      debugPrint("Failed to initialize object detector: $e");
+      rethrow; // rethrow the error to be caught by the FutureBuilder
+    }
+  }
+
+  Future<String> _copyModelToLocal(String assetPath) async {
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await io.Directory(dirname(path)).create(recursive: true);
+    final file = io.File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
+
+
+
+
+  Future<bool> _checkCameraPermission() async {
+    var cameraStatus = await Permission.camera.status;
+    if (!cameraStatus.isGranted) {
+      cameraStatus = await Permission.camera.request();
+    }
+    return cameraStatus.isGranted;
   }
 }
